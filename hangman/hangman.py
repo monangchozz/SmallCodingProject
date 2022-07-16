a67aff4a5d97dde7bc4d278f429b220bc455a738
monangchozz <hamonangan.jonathan@mail.ugm.ac.id>
2022-07-16 11:33:59 +0700

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,8 +15,6 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
-
-
     # getting user input
     while len(word_letters) > 0 :
         # letters used
